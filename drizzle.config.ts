2d622bae7003a7c42fb7d8ec5790f70021cc1e45
adamblumoff <ablumoff25@amherst.edu>
2025-12-19 22:55:46 -0600

    fixed build error

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -6,6 +6,6 @@ export default defineConfig({
   out: './db/migrations',
   dialect: 'postgresql',
   dbCredentials: {
-    connectionString: process.env.DATABASE_URL || ''
+    url: process.env.DATABASE_URL || ''
   }
 });
